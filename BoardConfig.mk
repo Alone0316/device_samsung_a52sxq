38bf9566437d1304547eef8a7a38468fa149d245
Alone0316 <rutujamandlik06@gmail.com>
2022-11-16 09:18:06 +0000

    a52sxq: Import modules.load from stock

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,6 +13,9 @@ TARGET_SCREEN_DENSITY := 450
 
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/mesa_a52sxq_eur_open_defconfig
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery))
+BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD)
 
 # Inherit the proprietary files
 include vendor/samsung/a52sxq/BoardConfigVendor.mk
